0c22415def90a5215dcbdbbb3ed801936e2beb0a
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2022-11-13 15:34:22 +0700

    update

--- Admin/app/src/main/java/com/edward/adminapp/LoginActivity.java
+++ Admin/app/src/main/java/com/edward/adminapp/LoginActivity.java
@@ -5,12 +5,18 @@ import static com.edward.adminapp.API.ServiceAPI.BASE_SERVICE;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.edward.adminapp.API.ServiceAPI;
+import com.edward.adminapp.adapter.ProgressDialogCustom;
 import com.edward.adminapp.model.modelrespon.ResGetListPerson;
 import com.edward.adminapp.model.modelrespon.ResGetPerson;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -26,54 +32,56 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
-import android.view.View;
-import android.widget.Button;
-
-public class LoginActivity extends AppCompatActivity {
 
     private Button btLogin;
+    private EditText edtEmailLogin;
+    private EditText edtPassword;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        DemoCallAPI();
-
         btLogin = findViewById(R.id.btLogin);
-
+        edtEmailLogin = findViewById(R.id.edtEmailLogin);
+        edtPassword = findViewById(R.id.edtPassword);
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                DemoCallAPI();
             }
         });
     }
 
+
     private void DemoCallAPI() {
-        ServiceAPI.serviceApi.GetAllPerson()
+        ServiceAPI.serviceApi.Login(edtEmailLogin.getText().toString(), edtPassword.getText().toString())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<ResGetListPerson>() {
+                .subscribe(new Observer<ResGetPerson>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-                        //progress dialog
+                        ProgressDialogCustom.showProgressDialog(LoginActivity.this, "please wait");
                     }
 
                     @Override
-                    public void onNext(ResGetListPerson resGetListPerson) {
-                        int k = resGetListPerson._PersonRes.size();
-                        Log.e(String.valueOf(k), "onNext: ");
+                    public void onNext(ResGetPerson resGetPerson) {
+                        if (resGetPerson._Respon.getRespone_code() != 200) {
+                            Toast.makeText(LoginActivity.this, "Check username, Psw pls", Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e(e.getMessage(),"log");
-                        Log.e("Ngu", "Log");
+                        ProgressDialogCustom.dismissProgressDialog();
+                        Toast.makeText(LoginActivity.this, "non null field", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onComplete() {
-                        //dismiss
+                        ProgressDialogCustom.dismissProgressDialog();
                     }
                 });
     }
