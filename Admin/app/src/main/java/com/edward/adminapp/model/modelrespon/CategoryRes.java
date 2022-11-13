519237350ec0a5b0106bb1e91b1313cd4596173d
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2022-11-13 13:07:32 +0700

    update

---
+++ Admin/app/src/main/java/com/edward/adminapp/model/modelrespon/CategoryRes.java
@@ -0,0 +1,8 @@
+package com.edward.adminapp.model.modelrespon;
+
+import java.io.Serializable;
+
+public class CategoryRes implements Serializable {
+    private int id;
+    private String name;
+}
