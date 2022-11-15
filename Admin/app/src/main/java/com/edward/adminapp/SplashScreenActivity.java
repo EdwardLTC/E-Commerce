28b663be8cbbb1c11c55d3554eaccfe9922a03df
PK02543baolinh <101194559+PK02543baolinh@users.noreply.github.com>
2022-11-15 10:38:51 +0700

    jhhh

--- Admin/app/src/main/java/com/edward/adminapp/SplashScreenActivity.java
+++ Admin/app/src/main/java/com/edward/adminapp/SplashScreenActivity.java
@@ -11,6 +11,5 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Log.e("ádf","ádfsdfa");
     }
 }
