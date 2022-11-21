22e2678ef8d20e086fcbf404dc4468ff2dcee2ae
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2022-11-21 22:54:36 +0700

    update API

---
+++ Seller_User/app/src/main/java/com/edward/myapplication/model/modelrespon/ResGetListProperties.java
@@ -0,0 +1,8 @@
+package com.edward.myapplication.model.modelrespon;
+
+import java.util.ArrayList;
+
+public class ResGetListProperties {
+    public Respon _Respon;
+    public ArrayList<ClothesPropertiesRes> _ClothesPropertiesRes;
+}
