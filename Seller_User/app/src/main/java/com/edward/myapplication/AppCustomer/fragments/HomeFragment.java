7543f7955e27911ba767774609b170e81598a612
vapnguyen24 <vinh.work@gmail.com>
2022-11-22 17:16:29 +0700

    commit

---
+++ Seller_User/app/src/main/java/com/edward/myapplication/AppCustomer/fragments/HomeFragment.java
@@ -0,0 +1,71 @@
+package com.edward.myapplication.AppCustomer.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.edward.myapplication.AppCustomer.views.Filtering;
+import com.edward.myapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeFragment extends Fragment {
+    BottomNavigationView bottomNavigationView;
+    ImageView img;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setItemIconTintList(null);
+        img = view.findViewById(R.id.filter);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Filtering filtering = new Filtering();
+                filtering.show(getActivity().getSupportFragmentManager(), "TAG");
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.bottom_home:
+                        break;
+                    case R.id.bottom_shop:
+                        break;
+                    case R.id.bottom_heart:
+                        break;
+                    case R.id.bottom_profile:
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame_home, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+}
