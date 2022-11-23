01cdad835f6bd84f47ba05650261b39a7dffb731
PK02543baolinh <101194559+PK02543baolinh@users.noreply.github.com>
2022-11-23 08:42:23 +0700

    SplashScreen_Admin

--- Admin/app/src/main/java/com/edward/adminapp/SplashScreenActivity.java
+++ Admin/app/src/main/java/com/edward/adminapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.edward.adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -11,5 +13,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                //finish
+                finish();
+            }
+        },1000);
     }
 }
