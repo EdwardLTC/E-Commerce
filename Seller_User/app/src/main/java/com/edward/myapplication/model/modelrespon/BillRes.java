f43cf7ad05e249273c4a5abe34c2b8a17e3d3d89
EdwardLTC <88788257+EdwardLTC@users.noreply.github.com>
2022-11-26 17:36:31 +0700

    update API

---
+++ Seller_User/app/src/main/java/com/edward/myapplication/model/modelrespon/BillRes.java
@@ -0,0 +1,10 @@
+package com.edward.myapplication.model.modelrespon;
+
+public class BillRes {
+    public int id;
+    public int iduser;
+    public int idseller;
+    public int idvoucher;
+    public String dateCreate;
+    public String status;
+}
