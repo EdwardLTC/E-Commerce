b5685e2133030b210d5360429c898e372005fe14
thanhhkhoi <khoinguyen2003hg@gmail.com>
2022-11-26 10:08:50 +0700

    2611

--- Seller_User/app/src/main/java/com/edward/myapplication/Customer/views/MainMycart.java
+++ Seller_User/app/src/main/java/com/edward/myapplication/Customer/views/MainMycart.java
@@ -1,15 +1,28 @@
 package com.edward.myapplication.Customer.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.edward.myapplication.R;
 
 public class MainMycart extends AppCompatActivity {
+    Button btncheckoutmycart, btnbackmycart;
+    RecyclerView recyclerView;
+    TextView tvpireprice2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_mycart);
+        btncheckoutmycart = findViewById(R.id.btncheckoutmycart);
+        btnbackmycart = findViewById(R.id.btnbackmycart);
+        tvpireprice2 = findViewById(R.id.tv_price2);
+        recyclerView = findViewById(R.id.RecyclerViewMyCart);
     }
 }
