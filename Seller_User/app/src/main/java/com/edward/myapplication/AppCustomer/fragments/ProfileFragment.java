92fab19af1006553f8c8b315c953c82802b047ef
msmci <102038721+msmci@users.noreply.github.com>
2022-11-28 11:39:37 +0700

    thao commit

--- Seller_User/app/src/main/java/com/edward/myapplication/AppCustomer/fragments/ProfileFragment.java
+++ Seller_User/app/src/main/java/com/edward/myapplication/AppCustomer/fragments/ProfileFragment.java
@@ -13,11 +13,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.edward.myapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class ProfileFragment extends Fragment {
     ImageView ic_more,iv_profile,ic_bag,ic_ticket,ic_star;
     TextView txtProfileName,txtProfileEmail,txtProfileProcessOrderNumber,txtProfilePromocodesNumber,txtProfileReviewesNumber,txtPIName,txtPIMail,txtPIAddess,txtPIPhoneNumber;
+    BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -45,5 +47,6 @@ public class ProfileFragment extends Fragment {
         txtPIMail=view.findViewById(R.id.txtPIMail);
         txtPIAddess=view.findViewById(R.id.txtPIAddress);
         txtPIPhoneNumber=view.findViewById(R.id.txtPIPhoneNumber);
+        bottomNavigationView= view.findViewById(R.id.bottomNavView);
     }
 }
