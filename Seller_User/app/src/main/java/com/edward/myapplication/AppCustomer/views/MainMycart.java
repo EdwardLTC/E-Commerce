95348073ec9d1251bceae8fe025f5265836c9593
vapnguyen24 <vinh.work@gmail.com>
2022-11-30 12:37:07 +0700

    commit

--- Seller_User/app/src/main/java/com/edward/myapplication/AppCustomer/views/MainMycart.java
+++ Seller_User/app/src/main/java/com/edward/myapplication/AppCustomer/views/MainMycart.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
---
+++ Seller_User/app/src/main/java/com/edward/myapplication/model/modelrespon/ResGetBillPayment.java
@@ -0,0 +1,6 @@
+package com.edward.myapplication.model.modelrespon;
+
+public class ResGetBillPayment {
+    public Respon _Respon;
+    public String _Payment;
+}
